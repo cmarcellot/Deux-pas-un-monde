f4478bf969e5240e69d9e9ce6df16c852de3bd5e
cmarcellot <camillemarcellot@gmail.com>
2026-06-01 19:50:40 +0200

    fix: add localhost to CORS origins

--- backend/server.py
+++ backend/server.py
@@ -24,6 +24,7 @@ app.add_middleware(
         "https://deuxpasunmonde.fr",
         "http://www.deuxpasunmonde.fr",
         "http://deuxpasunmonde.fr",
+	"http://localhost:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
